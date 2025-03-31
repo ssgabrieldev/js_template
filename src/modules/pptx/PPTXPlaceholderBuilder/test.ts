279b0e52ea7b35a3dfa32e7a71801c652ea5f229
gabriel <ssgabrieldev@gmail.com>
2025-03-30 22:54:13 -0300

    new: test pptx placeholder builder

--- src/modules/pptx/PPTXPlaceholderBuilder/test.ts
+++ src/modules/pptx/PPTXPlaceholderBuilder/test.ts
@@ -1,16 +1,40 @@
-import { DOMParser } from "xmldom";
-
 import { PPTXTextPlaceholder } from "../PPTXTextPlaceholder";
 import { PPTXPlaceholderBuilder } from ".";
+import { PPTXLoopPlaceholder } from "../PPTXLoopPlaceholder";
 
 describe("PPTXPlaceholder", () => {
   it("should return PPTXTextPlaceholder instance", () => {
     const placeholder = PPTXPlaceholderBuilder.new({
       key: "{foo}",
-      node: new DOMParser().parseFromString("")
+      node: {
+        cloneNode: () => {}
+      } as Element,
+      parent: null
     });
 
     expect(placeholder).toBeInstanceOf(PPTXTextPlaceholder);
+  });
+
+  it("should return PPTXLoopPlaceholder instance", () => {
+    const placeholder = PPTXPlaceholderBuilder.new({
+      key: "{#foo}",
+      node: {
+        cloneNode: () => {}
+      } as Element,
+      parent: null
+    });
+
+    expect(placeholder).toBeInstanceOf(PPTXLoopPlaceholder);
+  });
+
+  it("should return false to close tag", () => {
+    const placeholder = PPTXPlaceholderBuilder.new({
+      key: "{/foo}",
+      node: {} as Element,
+      parent: null
+    });
+
+    expect(placeholder).toBe(false);
   });
 });
 
