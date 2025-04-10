06aeaffa24da9cdd5801708efa34362671a3cd89
gabriel <ssgabrieldev@gmail.com>
2025-04-10 11:38:06 -0300

    new: make src/index.ts user interface

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,10 @@
-import { join } from "path";
-
+import { TemplateHandlerBuilder } from "./lib/TemplateHandlerBuilder";
 import { PPTXTemplateHandler } from "./modules/pptx/PPTXTemplateHandler";
-import { PPTXTemplateFile } from "./modules/pptx/PPTXTemplateFile";
-import { UtilsString } from "./modules/utils/string";
+import { PPTXSlide } from "./modules/pptx/PPTXSlide";
 
-const templateFile = new PPTXTemplateFile({
-  filePath: join(__dirname, "../assets/template.pptx")
-});
-const templateHandler = new PPTXTemplateHandler({
-  templateFile
-});
+export {
+  TemplateHandlerBuilder,
 
-async function main() {
-  const [_, error] = await templateHandler.save({
-    filePath: join(__dirname, "../assets/temp/template.pptx"),
-    data: {
-      title: "Teste",
-      name: "Paulo Gabriel Santana Silva",
-      modules: [
-        ["module1",
-        "module2"],
-        ["module3",
-        "module4",
-        "module5"]
-      ]
-    }
-  })
-
-  if (error) {
-    console.error(error)
-  }
-}
-
-main();
+  PPTXTemplateHandler,
+  PPTXSlide,
+};
